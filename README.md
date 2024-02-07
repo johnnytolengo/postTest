95e6d24324214ddedbe6d6b6d8b375faa6a5d921
devel01 <devel01@quark-i.com>
2024-02-07 16:23:30 +0100

    ready for testing

---
+++ README.md
@@ -0,0 +1,5 @@
+
+## Device ip address
+set the device ip address on package.json  "proxy"
+
+  
--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import React, { useEffect } from 'react';
+
 import Axios from "axios";
 import "./styles.css";
+import nerdImg from "./images/nerd.png";
 
-const FILES = [{ filename: "file1.txt", data: "" }];
+const FILES = [
+  { filename: "file1.png"},
+  { filename: "file2.png"},
+  { filename: "file3.png"},
+  { filename: "file4.png"},
+  { filename: "file5.png"},
+];
 export default function App() {
+
+  const [bindata, setBindata] = React.useState(null);
+
+  useEffect(() => {
+    fetchAndCovertToBlob(nerdImg);
+  }, []);
+
+
+  const fetchAndCovertToBlob = (url)=> {
+    fetch(url)
+    .then( response => response.blob())
+    .then( blobData => {
+        //const {size, type} = blobData
+        setBindata(blobData)
+    });
+  }
+
+
   async function postFile(filename, url_path, payload) {
     const config = {
       headers: {
@@ -13,11 +40,32 @@ export default function App() {
       },
     };
 
-    return await Axios.post(APIURL + url_path, payload, config);
+    return await Axios.post(url_path, payload, config);
   }
 
-  const handleSendFiles = () => {
-    console.log("sending...");
+  const handleSendFiles = async () => {
+    
+    
+    FILES.map(async (item, index)  =>  {
+      console.log("sending: ", item.filename )
+
+      let formData = new FormData();
+      formData.append('blob', bindata);
+      formData.append('ord', index);
+      formData.append('size', bindata.size);
+
+      
+      await postFile(item.filename, "/upload", formData)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("Sent ", item.filename, " success!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error sending: ", item.filename);
+      });
+      
+    });
   };
 
   return (
